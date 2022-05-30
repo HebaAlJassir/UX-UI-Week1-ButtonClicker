9d1e824e23a83a919ca72bbe52b33e6443de1086
Heba <bon3bona@gmail.com>
2022-05-30 13:06:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function logout(element) {
+    element.innerText = "Logout";
+}
+
+function Likes() {
+    alert("Ninja was liked ");
+}
+function Remove(el) {
+    var element = el;
+    element.remove();
+  }
